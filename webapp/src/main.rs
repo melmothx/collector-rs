bd6c75ec786c15f3e680cb15503cdf0631b1422b
Marco Pessotto <marco@pessotto.hr>
2025-07-23 11:32:52 +0200

    Implement facets

--- webapp/src/main.rs
+++ webapp/src/main.rs
@@ -22,10 +22,36 @@ struct Entry {
     title: String,
 }
 
+#[derive(Serialize, Debug)]
+struct Facet {
+    count: i64,
+    term: String,
+    id: String,
+}
+
+#[derive(Serialize, Debug)]
+struct FacetList {
+    name: String,
+    values: Vec<Facet>,
+}
+
+#[derive(Serialize, Debug)]
+struct FacetBlock {
+    library: FacetList,
+    creator: FacetList,
+    language: FacetList,
+}
+
+#[derive(Serialize, Debug)]
+struct SearchResult {
+    entries: Vec<Entry>,
+    facets: FacetBlock,
+}
+
 async fn search(
     State(pool): State<ConnectionPool>,
     Query(params): Query<HashMap<String, String>>,
-) -> (StatusCode, Json<Vec<Entry>>) {
+) -> (StatusCode, Json<SearchResult>) {
     let conn = pool.get().await.expect("Failed to get a connection from the pool");
     let sql = r#"
 SELECT entry_id, title, ts_rank_cd(search_vector, query) AS rank
@@ -33,13 +59,12 @@ FROM entry, websearch_to_tsquery($1) query
 WHERE search_vector @@ query
 ORDER BY rank DESC
 LIMIT 10;
-
 "#;
     let query = match params.get("query") {
         Some(value) => value,
         None => "",
     };
-    let out = conn.query(sql, &[&query]).await.expect("Query failed")
+    let out = conn.query(sql, &[&query]).await.expect("Query should be valid")
         .iter().map(|row|
                     Entry {
                         entry_id: row.get(0),
@@ -47,7 +72,85 @@ LIMIT 10;
                         rank: row.get(2),
                     }).collect();
     tracing::debug!("{:?}", &out);
-    (StatusCode::OK, Json(out))
+
+    let lang_sql = r#"
+SELECT count(*) AS count,
+       COALESCE(l.native_name, l.english_name, l.language_code) AS term,
+       l.language_code AS id
+FROM entry e
+JOIN entry_language el ON el.entry_id = e.entry_id
+JOIN known_language l ON l.language_code = el.language_code
+WHERE websearch_to_tsquery($1) @@ e.search_vector
+GROUP BY l.language_code, l.native_name, l.english_name
+ORDER BY count(*) DESC
+"#;
+    let langs = conn.query(lang_sql, &[&query]).await.expect("Query should be valid")
+        .iter().map(|row|
+                    Facet {
+                        count: row.get(0),
+                        term: row.get(1),
+                        id: row.get(2),
+                    }).collect();
+
+    let authors_sql = r#"
+SELECT count(*) AS count,
+       a.full_name AS term,
+       a.agent_id::TEXT AS id
+FROM entry e
+JOIN entry_agent ea ON ea.entry_id = e.entry_id
+JOIN agent a ON a.agent_id = ea.agent_id
+WHERE websearch_to_tsquery($1) @@ e.search_vector
+GROUP BY a.full_name, a.agent_id
+ORDER BY count(*) DESC
+"#;
+    let authors = conn.query(authors_sql, &[&query]).await.expect("Query should be valid")
+        .iter().map(|row|
+                    Facet {
+                        count: row.get(0),
+                        term: row.get(1),
+                        id: row.get(2),
+                    }).collect();
+
+    let libraries_sql = r#"
+SELECT count(*) AS count,
+       l.name AS term,
+       l.library_id::TEXT AS id
+FROM entry e
+JOIN datasource ds ON e.entry_id = ds.entry_id
+JOIN site s ON s.site_id = ds.site_id
+JOIN library l ON s.library_id = l.library_id
+WHERE websearch_to_tsquery($1) @@ e.search_vector
+GROUP BY l.name, l.library_id
+ORDER BY count(*) DESC
+"#;
+
+    let libraries = conn.query(libraries_sql, &[&query]).await.expect("Query should be valid")
+        .iter().map(|row|
+                    Facet {
+                        count: row.get(0),
+                        term: row.get(1),
+                        id: row.get(2),
+                    }).collect();
+
+
+
+    (StatusCode::OK, Json(SearchResult {
+        entries: out,
+        facets: FacetBlock {
+            language: FacetList {
+                name: String::from("language"),
+                values: langs,
+            },
+            creator: FacetList {
+                name: String::from("creator"),
+                values: authors,
+            },
+            library: FacetList {
+                name: String::from("libraryr"),
+                values: libraries,
+            },
+        }
+    }))
 }
 
 #[tokio::main]
